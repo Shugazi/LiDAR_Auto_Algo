cc4b0eceab3b8c5ea825a157b15c001b7105e80b
Oscar Resendiz <resendizor17@gmail.com>
2023-02-04 18:07:47 -0600

    Added directions

--- auto_Algo.py
+++ auto_Algo.py
@@ -14,9 +14,24 @@ try:
         num += 1
         hazard_detected = False
         for intensity, angle, distance in scan:
+
             # check if there is a hazard
             if distance <= threshold_distance:
                 hazard_detected = True
+                print(angle)
+
+                if 0 <= angle < 45:
+                    print("Hazard detected to the right front")
+                    print("Moving to the Left \n")
+
+                elif 45 <= angle < 90:
+                    print("Hazard detected to the front")
+                    print("Moving to the Right\n")
+
+                elif 90 <= angle < 135:
+                    print("Hazard detected to the left front")
+                    print("Moving to the Right\n")
+
                 # convert angle to servo duty cycle
                 print("Avoding obstacle\n")
         # drive forward if no hazards are detected

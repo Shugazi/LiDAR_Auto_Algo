5b75557d50ff0c8807269cec2e5161e19d6dd14c
Oscar Resendiz <resendizor17@gmail.com>
2023-04-19 22:55:00 -0500

    added a new measure_new.py file

---
+++ measure_new.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+'''Records measurments to a given file. Usage example:
+
+$ ./record_measurments.py out.txt'''
+import sys
+from rplidar import RPLidar
+import json
+
+PORT_NAME = '/dev/ttyUSB0'
+
+
+def run(path):
+    '''Main function'''
+    lidar = RPLidar(PORT_NAME)
+    outfile = open(path, 'w')
+    try:
+        print('Recording measurments... Press Crl+C to stop.')
+        for measurment in lidar.iter_measurments():
+            # We need a data structure so we will use dictionary
+            data = {
+                'angle': measurment[1],
+                'distance': measurment[2],
+                'quality': measurment[3],
+                'timestamp': measurment[0],
+            }
+            # Write the dictionary as a JSON string to the file
+            outfile.write(json.dumps(data) + '\n')
+
+    except KeyboardInterrupt:
+        print('Stoping.')
+    lidar.stop()
+    lidar.disconnect()
+    outfile.close()
+
+
+if __name__ == '__main__':
+    run(sys.argv[1])
